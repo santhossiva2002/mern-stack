990a4fa70f05e672bffca44fb7d602d677f42217
santhossiva2002 <143084051+santhossiva2002@users.noreply.github.com>
2023-12-17 00:43:42 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -39,7 +39,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/signup",
+        "https://mern-stack-phi-topaz.vercel.app/signup",
         {
           ...inputValue,
         },
